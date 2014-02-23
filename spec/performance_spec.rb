fbdc8937bed8ab0ef3ed06660a114222350b292e
Jamie Dyer <jamie@kernowsoul.com>
2014-02-23 21:27:49 +0000

    Chore: added a performance spec (commented out as long running)

---
+++ spec/performance_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'parslet/rig/rspec'
+require 'pp'
+require 'benchmark'
+
+# Uncomment to run performace specs
+
+
+# describe Shortcode do
+
+#   let(:long_text)   { load_fixture :long_text }
+#   let(:parser)      { Shortcode::Parser.new }
+#   let(:transformer) { Shortcode::Transformer.new }
+
+#   context "parser" do
+
+#     before { long_text }
+
+#     it "runs quickly" do
+#       Benchmark.realtime {
+#         parser.parse long_text
+#       }.should < 1.5
+#     end
+
+#   end
+
+#   context "transformer" do
+
+#     let(:parsed_hash) { parser.parse long_text }
+
+#     before { parsed_hash }
+
+#     it "runs quickly" do
+#       Benchmark.realtime {
+#         transformer.apply(parsed_hash)
+#       }.should < 0.1
+#     end
+
+#   end
+
+# end
