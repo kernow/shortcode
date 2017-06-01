1c56139b524e11dbb65d0df1fd5dd6028b0ac603
Todd D Nestor <todd.nestor@gmail.com>
2017-06-01 11:52:50 -0700

    Template from configuration or file (#48)
    * Allow templates from configuration and templates to both be used
    * Updated readme

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,8 @@ RSpec.configure do |config|
     Shortcode.setup do |config|
       config.template_parser = :erb
       config.template_path = File.join File.dirname(__FILE__), "support/templates/erb"
-      config.templates = nil
+      config.templates = {}
+      config.check_config_templates_first = true
       config.block_tags = [:quote, :collapsible_list, :item, :timeline_person, :rails_helper, :custom_helper]
       config.self_closing_tags = [:timeline_event, :timeline_info]
       config.attribute_quote_type = '"'
